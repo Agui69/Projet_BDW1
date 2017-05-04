8ed7905d97a3644f50efee8160011caf12dc23dd
Agui69 <bacarcomcom@yahoo.fr>
2017-05-04 21:53:41 +0200

    Update init.php

--- init.php
+++ init.php
@@ -1,10 +1,10 @@
 <?php
 $bd = ’L2IF07_BD ’ ;
-$user = ’<p1503762>’  ;
-$passwd = ’<Lilkha04>’  ;
+$user = ’p1503762’  ;
+$passwd = ’Lilkha04’  ;
 $machine = ’ localhost ’ ;
 $connexion = mysqli_connect ( $machine , $user , $passwd , $bd ) ;
-i f
+if
 ( mysqli_connect_errno ()){
 //  erreur  s i > 0
 printf
